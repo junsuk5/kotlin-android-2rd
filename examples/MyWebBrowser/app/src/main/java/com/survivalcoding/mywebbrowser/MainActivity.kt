1a10e571f8c4f12939943f74aca51ff38770777e
Junsuk Oh <a811219@gmail.com>
2021-08-12 19:42:02 +0900

    7.4.2 웹뷰에 웹 페이지 표시하기

--- examples/MyWebBrowser/app/src/main/java/com/survivalcoding/mywebbrowser/MainActivity.kt
+++ examples/MyWebBrowser/app/src/main/java/com/survivalcoding/mywebbrowser/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.survivalcoding.mywebbrowser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import com.survivalcoding.mywebbrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // 웹뷰 기본 설정
+        binding.webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView, url: String) {
+                    binding.urlEditText.setText(url)
+                }
+            }
+        }
+
+        binding.webView.loadUrl("https://www.google.com")
     }
 }
