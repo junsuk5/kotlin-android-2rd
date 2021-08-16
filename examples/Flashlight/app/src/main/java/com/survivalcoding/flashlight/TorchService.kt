0016cc2c375166f80f55c996922d0b53e9e52d8d
Junsuk Oh <a811219@gmail.com>
2021-08-16 14:10:20 +0900

    12.3 스텝1 레이아웃 작성

--- examples/Flashlight/app/src/main/java/com/survivalcoding/flashlight/TorchService.kt
+++ examples/Flashlight/app/src/main/java/com/survivalcoding/flashlight/TorchService.kt
@@ -9,14 +9,27 @@ class TorchService : Service() {
         Torch(this)
     }
 
+    private var isRunning = false
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.action) {
             // 앱에서 실행할 경우
             "on" -> {
                 torch.flashOn()
+                isRunning = true
             }
             "off" -> {
                 torch.flashOff()
+                isRunning = false
+            }
+            // 서비스에서 실행할 경우
+            else -> {
+                isRunning = !isRunning
+                if (isRunning) {
+                    torch.flashOn()
+                } else {
+                    torch.flashOff()
+                }
             }
         }
         return super.onStartCommand(intent, flags, startId)
